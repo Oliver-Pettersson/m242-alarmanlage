9a5d93f7a82cba044bd532391287c118c904004e
Philipp Albrecht <philipp@uisa.ch>
2022-03-22 00:28:45 +0100

    Added Telegram bot example

--- src/main/java/ch/alptbz/mqttdemo/Main.java
+++ src/main/java/ch/alptbz/mqttdemo/Main.java
@@ -1,8 +1,14 @@
 package ch.alptbz.mqttdemo;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.function.BiConsumer;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
@@ -12,13 +18,36 @@ public class Main {
 
     public static double temperature = 0;
     public static double humidity = 0;
+    private static Logger logger;
+    private static Properties config;
+
+    private static boolean loadConfig() {
+        config = new Properties();
+        try {
+            config.load(new FileReader("config.properties"));
+            return true;
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error loading config file",e);
+        }
+        return false;
+    }
 
     public final static void main(String[] args) throws InterruptedException {
         ConsoleHandler ch = new ConsoleHandler();
         ch.setLevel(Level.ALL);
         Logger.getGlobal().addHandler(ch);
 
-        Mqtt mqttClient = new Mqtt("tcp://cloud.tbz.ch:1883", "runner-1");
+        logger = Logger.getLogger("main");
+
+        if(!loadConfig()) return;
+
+        logger.info("Config file loaded");
+
+        TelegramNotificationBot tnb = new TelegramNotificationBot(config.getProperty("telegram-apikey"));
+
+        logger.info("TelegramBot started");
+
+        Mqtt mqttClient = new Mqtt(config.getProperty("mqtt-url"), "runner-1");
         try {
             mqttClient.start();
             mqttClient.subscribe("m5core2/#");
@@ -42,11 +71,9 @@ public class Main {
         double lastTemperature = temperature;
 
         while(true) {
-            if(Math.abs(lastTemperature - temperature) > 2)  {
+            if(Math.abs(lastTemperature - temperature) >= 1)  {
                 System.out.println("Temperature changed. Current: %.2f".formatted(temperature));
-                if(temperature > 24) {
-                    System.out.println("Temperature too high!");
-                }
+                tnb.sendTemperatureNotificationToAllUsers(temperature);
                 lastTemperature = temperature;
             }
             Thread.sleep(1000);
---
+++ src/main/java/ch/alptbz/mqttdemo/TelegramNotificationBot.java
@@ -0,0 +1,65 @@
+package ch.alptbz.mqttdemo;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TelegramNotificationBot
+extends Thread implements UpdatesListener {
+
+    private final TelegramBot bot;
+    private final ArrayList<Long> users = new ArrayList<>();
+
+    public TelegramNotificationBot(String botToken) {
+        bot = new TelegramBot(botToken);
+
+        bot.setUpdatesListener(this);
+    }
+
+    public void sendTemperatureNotificationToAllUsers(double temperature) {
+        for(Long user: users) {
+            SendMessage reply = new SendMessage(user, "The temperature changed to %.2f °C".formatted(temperature));
+            bot.execute(reply);
+        }
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        for(Update update: updates) {
+            String message = update.message().text();
+            if(message.startsWith("/help")) {
+                SendMessage reply = new SendMessage(update.message().chat().id(), "Use /subscribe to subscribe to temperature updates. Use /unsubscribe to leave");
+                bot.execute(reply);
+            }
+            if(message.startsWith("/subscribe")) {
+                if(!users.contains(update.message().chat().id())) {
+                    users.add(update.message().chat().id());
+                    SendMessage reply = new SendMessage(update.message().chat().id(),
+                            "Welcome! Use /unsubscribe to stop getting notifications.");
+                    bot.execute(reply);
+                }else{
+                    SendMessage reply = new SendMessage(update.message().chat().id(),
+                            "You are already subscribed the temperature notifications!");
+                    bot.execute(reply);
+                }
+            }
+            if(message.startsWith("/unsubscribe")) {
+                if(users.contains(update.message().chat().id())) {
+                    users.remove(update.message().chat().id());
+                    SendMessage reply = new SendMessage(update.message().chat().id(),
+                            "Byebye!");
+                    bot.execute(reply);
+                }else{
+                    SendMessage reply = new SendMessage(update.message().chat().id(),
+                            "You cannot unsubscribe something you've never subscribed to.");
+                    bot.execute(reply);
+                }
+            }
+        }
+
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+}
